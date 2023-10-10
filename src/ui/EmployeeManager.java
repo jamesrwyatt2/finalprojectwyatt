b257111d1a7ee37ca3249d2c0ad08a7a09226866
James Wyatt <jamesrwyatt35@gmail.com>
2023-10-10 00:29:39 -0400

    Employee Tab UI built

--- src/ui/EmployeeManager.java
+++ src/ui/EmployeeManager.java
@@ -5,6 +5,16 @@ import javax.swing.*;
 public class EmployeeManager {
     private JTabbedPane tabbedPane1;
     private JPanel rootPanel;
+    private JTable table1;
+    private JTextField firstName;
+    private JTextField lastName;
+    private JTextField pcHr;
+    private JTextField networkHr;
+    private JTextField cableHr;
+    private JTextField pcYr;
+    private JTextField networkYr;
+    private JTextField cableYr;
+    private JButton createButton;
 
 
     public JPanel getRootPanel() {
